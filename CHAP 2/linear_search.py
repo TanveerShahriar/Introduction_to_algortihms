cc2d0ae982ec993f927da5a954c40fefd3307ffc
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2024-01-23 13:06:03 +0600

    linear search updated

--- CHAP 2/linear_search.py
+++ CHAP 2/linear_search.py
@@ -1,3 +1,11 @@
+# Time Complexity:
+
+# Best  Case : O(1)
+# Avg   Case : O(n)
+# Worst Case : O(n)
+
+# Space Complexity: O(1) (Always)
+
 def linear_search(arr, val):
     for i in range(len(arr)):
         if arr[i] == val:
