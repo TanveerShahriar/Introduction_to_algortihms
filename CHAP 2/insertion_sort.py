7ae26881227435a69e74c541bf88750552e96cd9
Tanveer Shahriar Arnob <tanveershahriara@gmail.com>
2024-05-31 07:06:18 +0600

    recursive insertion sort completed

--- CHAP 2/insertion_sort.py
+++ CHAP 2/insertion_sort.py
@@ -19,8 +19,30 @@ def insertion_sort(arr, reverse = False):
         
         arr[idx + 1] = temp
 
+def insertion_sort_recursive(arr, i = 1, reverse = False):
+    if len(arr) == i:
+        return
+    
+    temp = arr[i]
+    idx = i - 1
+    
+    while idx >= 0 and ((reverse and arr[idx] < temp) or (not reverse and arr[idx] > temp)):
+        arr[idx + 1] = arr[idx]
+        idx -= 1
+    
+    arr[idx + 1] = temp 
+    
+    insertion_sort_recursive(arr, i + 1, reverse)
+    
+
 arr = [10, 9, 8, 7, 6, 5]
 insertion_sort(arr)
 print(arr)
 insertion_sort(arr, reverse = True)
-print(arr)       
+print(arr)  
+
+arr = [10, 9, 8, 7, 6, 5]
+insertion_sort_recursive(arr)
+print(arr)
+insertion_sort_recursive(arr, reverse = True)
+print(arr)  
